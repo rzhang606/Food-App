252ed4d3168e9fdb585cea4b508f9bcd33262459
rzhang606 <rzhang606@gmail.com>
2019-03-28 19:37:13 -0400

    Location works correctly now, requires Google Services to be active. Using FusedLocationProviderClient Added dependencies for google services permissions change small ui changes

--- app/src/main/java/com/example/android/letseat/mainMenu.java
+++ app/src/main/java/com/example/android/letseat/mainMenu.java
@@ -1,19 +1,35 @@
 package com.example.android.letseat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
 
 public class mainMenu extends AppCompatActivity {
 
     private static final String LOG_TAG = mainMenu.class.getSimpleName();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
     }
 
     /**
--- app/src/main/java/com/example/android/letseat/surpriseMe.java
+++ app/src/main/java/com/example/android/letseat/surpriseMe.java
@@ -1,13 +1,26 @@
 package com.example.android.letseat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.SparseArray;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -28,22 +41,77 @@ import java.util.Random;
 
 public class surpriseMe extends AppCompatActivity {
 
-    LocationManager currentLocation;
-
     private static final String LOG_TAG = surpriseMe.class.getSimpleName();
+    private static final int LOCATION_REQUEST_CODE = 1000;
 
-    HashMap<Integer, Business> map = new HashMap<>();
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location myLocation;
+
+    SparseArray<Business> bArray = new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_surprise_me);
-        currentLocation = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        //get data from yelp api
-        FetchDataAsyncTask task = new FetchDataAsyncTask();
-        task.execute();
+        getLocation();
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                // If the permission is granted, get the location,
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    Log.d(LOG_TAG, "Failed to get permissions");
+                }
+                break;
+        }
+    }
+
+    /**
+     * Retrieves Location and stores in myLocation variable
+     * Once location is retrieved, fires off the Async Task for API call
+     */
+    private void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+        } else {
+            Log.d(LOG_TAG, "getLocation: permissions granted");
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        Task<Location> task = mFusedLocationClient.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    myLocation = location;
+                    Log.d(LOG_TAG, "My location is " + myLocation.toString());
+                } else {
+                    Log.d(LOG_TAG, "Location finding error: NULL retrieved");
+                }
+            }
+        });
+
+        task.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //get data from yelp api
+                FetchDataAsyncTask fetchDataTask = new FetchDataAsyncTask();
+                fetchDataTask.execute();
+            }
+        });
+
+    }
+
 
 
     public class FetchDataAsyncTask extends AsyncTask<URL, Void, String> {
@@ -51,8 +119,16 @@ public class surpriseMe extends AppCompatActivity {
         @Override
         protected String doInBackground(URL... url) {
 
+            URL generalURL = null;
+
             //Create URL object
-            URL generalURL = createUrl(getString(R.string.businessSearchURL));
+            try {
+                generalURL = createUrl(getString(R.string.myLocationSearch) + "?latitude=" + myLocation.getLatitude() + "&longitude=" + myLocation.getLongitude());
+                Log.d(LOG_TAG, "URL created: " + generalURL.toString());
+            } catch (NullPointerException e) {
+                Log.d(LOG_TAG, "mylocation is null");
+            }
+
 
             // Perform HTTP request to the URL and receive a JSON response back
             String jsonResponse = "";
@@ -75,29 +151,27 @@ public class surpriseMe extends AppCompatActivity {
 
             TextView display = findViewById(R.id.resultsText);
             Log.d(LOG_TAG, "My Random Number: " + myRand);
-            display.setText(map.get(myRand).toString());
+            display.setText(bArray.get(myRand).toString());
         }
 
         /**
          * Parse Json data into something useable
          */
-        private String parseJson(String rawdata) {
+        private void parseJson(String rawData) {
             try {
 
-                JSONObject json = new JSONObject(rawdata);
+                JSONObject json = new JSONObject(rawData);
                 JSONArray businessArray = json.getJSONArray("businesses");
 
                 //turn jsonarray which represents businesses list into map of Business class
-                for (int i = 0; i < businessArray.length(); i++) {
+                for (int i = 0; i < businessArray.length(); i++)
                     insertBusiness((JSONObject) businessArray.get(i), i);
-                }
 
             } catch (JSONException e) {
                 Log.d(LOG_TAG, "Parse JSON Error");
                 e.printStackTrace();
             }
 
-            return "";
         }
 
         /**
@@ -135,8 +209,8 @@ public class surpriseMe extends AppCompatActivity {
                         obj.getString("phone"),
                         obj.getDouble("distance")
                 );
-                map.put(index, business);
-                Log.d(LOG_TAG, "Businesses Count: " + map.size());
+                bArray.append(index, business);
+                Log.d(LOG_TAG, "Businesses Count: " + bArray.size());
             } catch (JSONException e) {
                 Log.d(LOG_TAG, "Insert business failed...");
                 e.printStackTrace();
@@ -152,7 +226,7 @@ public class surpriseMe extends AppCompatActivity {
             try {
                 url = new URL(stringUrl);
             } catch (MalformedURLException exception) {
-                Log.e(LOG_TAG, "Error with creating URL", exception);
+                Log.e(LOG_TAG, "Error with creating URL: " + stringUrl, exception);
                 return null;
             }
             return url;
