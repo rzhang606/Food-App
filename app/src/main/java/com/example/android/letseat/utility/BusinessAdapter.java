1fc162f02c1b97779e77856c5dd0cc6a1249db94
Richard Zhang <rzhang@inflightcorp.com>
2019-11-01 10:55:34 -0400

    backend: migrated to androidx

--- app/src/main/java/com/example/android/letseat/utility/BusinessAdapter.java
+++ app/src/main/java/com/example/android/letseat/utility/BusinessAdapter.java
@@ -1,8 +1,8 @@
 package com.example.android.letseat.utility;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +12,6 @@ import android.widget.TextView;
 
 import com.example.android.letseat.Business;
 import com.example.android.letseat.R;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
