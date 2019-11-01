4e8e5ada61e20dbb7f8570d12f2909c82d4cbc8e
Richard <rzhang606@gmail.com>
2019-10-31 20:00:11 -0400

    UI: more icons added for list

--- app/src/main/java/com/example/android/letseat/utility/BusinessAdapter.java
+++ app/src/main/java/com/example/android/letseat/utility/BusinessAdapter.java
@@ -3,7 +3,6 @@ package com.example.android.letseat.utility;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,22 +34,56 @@ public class BusinessAdapter extends ArrayAdapter<Business> {
         bArray = list;
 
         category_to_icon.put("Burgers", R.drawable.burger);
+
         category_to_icon.put("Beer", R.drawable.beer);
         category_to_icon.put("Breweries", R.drawable.beer);
+        category_to_icon.put("Bars", R.drawable.beer);
+        category_to_icon.put("Pubs", R.drawable.beer);
+
         category_to_icon.put("Bakery", R.drawable.cookie);
+        category_to_icon.put("Bakeries", R.drawable.cookie);
+
         category_to_icon.put("Desserts", R.drawable.cake);
+
         category_to_icon.put("Breakfast & Brunch", R.drawable.breakfast);
+
         category_to_icon.put("Candy", R.drawable.candy);
+
         category_to_icon.put("Coffee & Tea", R.drawable.coffee);
         category_to_icon.put("Coffee Roasteries", R.drawable.coffee);
+
         category_to_icon.put("Steak House", R.drawable.meat);
+
+        category_to_icon.put("Noodles", R.drawable.noodles);
+
+        category_to_icon.put("Salad", R.drawable.salad);
+
         category_to_icon.put("Pizza", R.drawable.pizza);
+
         category_to_icon.put("Pasta", R.drawable.spaguetti);
+
         category_to_icon.put("Japanese", R.drawable.sushi);
+        category_to_icon.put("Sushi Bars", R.drawable.sushi);
+
         category_to_icon.put("Tea", R.drawable.tea);
-        category_to_icon.put("Fruit", R.drawable.watermelon);
+        category_to_icon.put("Tea Rooms", R.drawable.tea);
         category_to_icon.put("Bubble Tea", R.drawable.boba);
+
+
+        category_to_icon.put("Fruit", R.drawable.watermelon);
+
         category_to_icon.put("Seafood", R.drawable.crab);
+
+        category_to_icon.put("Sandwiches", R.drawable.sandwich);
+
+        category_to_icon.put("Soup", R.drawable.soup);
+
+        category_to_icon.put("Middle Eastern", R.drawable.kebab);
+        category_to_icon.put("Falafel", R.drawable.kebab);
+        category_to_icon.put("Lebanese", R.drawable.kebab);
+
+        category_to_icon.put("Bagel", R.drawable.bagel);
+
     }
 
     @Override
@@ -65,7 +98,7 @@ public class BusinessAdapter extends ArrayAdapter<Business> {
         Business mBusiness = bArray.get(position);
 
         ImageView icon = v.findViewById(R.id.row_Icon);
-        int icon_image_id = R.drawable.cookie;
+        int icon_image_id = R.drawable.mdefault;
         for(String category: mBusiness.getCategories()) {
             if(category_to_icon.get(category) != null) {
                 icon_image_id = category_to_icon.get(category);
