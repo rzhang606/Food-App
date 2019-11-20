bd8ebe67b090c48c55962d91a0c9df212ab7dd01
Richard Zhang <rzhang@inflightcorp.com>
2019-11-20 16:37:42 -0500

    memory leak fix 2

--- app/src/main/java/com/example/android/letseat/utility/FetchLocation.java
+++ app/src/main/java/com/example/android/letseat/utility/FetchLocation.java
@@ -14,22 +14,27 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.Task;
 
+import java.lang.ref.WeakReference;
+
 public class FetchLocation {
 
     private static final int LOCATION_REQUEST_CODE = 1000;
     private final String LOG_TAG = FetchLocation.class.getSimpleName();
-    private Context context;
-    private Activity activity;
+    private WeakReference<Context> weak_context;
+    private WeakReference<Activity> weak_activity;
 
     public FetchLocation(Context context, Activity activity) {
-        this.context = context;
-        this.activity = activity;
+        this.weak_context = new WeakReference<>(context);
+        this.weak_activity = new WeakReference<>(activity);
     }
 
     /**
      * Retrieves Location and stores in myLocation variable
      */
     public Task<Location> getLocation() {
+
+        Context context = weak_context.get();
+        Activity activity = weak_activity.get();
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
