c423e1a2e4387676e898e9a04a8fe2ccd3c75f2a
Richard Zhang <rzhang@inflightcorp.com>
2019-11-22 13:55:22 -0500

    Sort feature addeed: works with distance and price :#28

--- app/src/main/java/com/example/android/letseat/activities/BusinessListView.java
+++ app/src/main/java/com/example/android/letseat/activities/BusinessListView.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.example.android.letseat.BottomNavigationActivity;
 import com.example.android.letseat.Business;
@@ -19,11 +20,16 @@ import com.example.android.letseat.R;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class BusinessListView extends BottomNavigationActivity {
 
     private final String LOG_TAG = BusinessListView.class.getSimpleName();
     BottomSheetBehavior sheetBehavior;
+    Spinner spinner;
+    int currentSpinnerItem = 0;
     ListView myListView;
     ArrayList<Business> bArray = new ArrayList<>();
 
@@ -49,6 +55,11 @@ public class BusinessListView extends BottomNavigationActivity {
 
         //Set up the bottom sheet
         setUpBottomSheet(R.id.list_coordinator_layout, R.id.list_content_layout);
+
+        //Spinner
+        spinner = findViewById(R.id.list_spinner);
+        setUpSpinner(spinner);
+
 
 
     }
@@ -103,4 +114,66 @@ public class BusinessListView extends BottomNavigationActivity {
         });
     }
 
+    private void setUpSpinner(Spinner spinner) {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                if(i == currentSpinnerItem) {
+                    return;
+                }
+
+                String selectedItem = adapterView.getItemAtPosition(i).toString();
+                if(selectedItem.equals("Distance")) {
+                    bArray = sortList(bArray, "distance");
+                } else if (selectedItem.equals("Price")) {
+                    bArray = sortList(bArray, "price");
+                }
+                setUpList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private ArrayList<Business> sortList(ArrayList<Business> bArr, String criteria){
+        Comparator<Business> comparator;
+        if(criteria.equals("distance")) {
+            comparator = new Comparator<Business>() {
+                @Override
+                public int compare(Business business, Business t1) {
+                    double b1 = business.getDistance();
+                    double b2 = t1.getDistance();
+                    if(b1 > b2){
+                        return 1;
+                    } else if (b1 < b2){
+                        return -1;
+                    }
+                    return 0;
+                }
+            };
+        } else { //price
+            comparator = new Comparator<Business>() {
+                @Override
+                public int compare(Business business, Business t1) {
+                    int len1 = business.getPrice().length();
+                    int len2 = t1.getPrice().length();
+                    if(len1 > len2) {
+                        return 1;
+                    } else if (len1 < len2){
+                        return -1;
+                    }
+                    return 0;
+                }
+            };
+        }
+
+        Collections.sort(bArr, comparator);
+
+        return bArr;
+
+    }
 }
