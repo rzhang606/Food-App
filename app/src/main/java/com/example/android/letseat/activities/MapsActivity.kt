df4dd8a6892e88692abba1bba29c8a64e1bbca1f
Richard Zhang <rzhang@inflightcorp.com>
2019-12-06 15:18:51 -0500

    added bottomsheet behavior to maps

--- app/src/main/java/com/example/android/letseat/activities/MapsActivity.kt
+++ app/src/main/java/com/example/android/letseat/activities/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.example.android.letseat.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.example.android.letseat.BottomNavigationActivity
 
 import com.example.android.letseat.R
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,14 +16,29 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : BottomNavigationActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    //UI Elements
+    lateinit var searchText : EditText
+    lateinit var searchButton : Button
+    lateinit var sheetBehavior : BottomSheetBehavior<LinearLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        super.setNavigationListener()
+
+        //Grab UI Elements
+        searchText = findViewById(R.id.map_search_query)
+        searchButton = findViewById(R.id.map_search_button)
+
+        //Bottom Sheet Setup
+        setUpBottomSheet(R.id.map_coordinator_layout, R.id.map_content_layout)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
@@ -40,5 +61,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    /**
+     * Switches between expanded and half, bottom sheet locked otherwise
+     */
+    private fun toggleFilters() {
+        if (sheetBehavior.state == BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED)
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        }
+    }
+
+    /**
+     * Sets up the bottom sheet (which contains the list)
+     * @param coordLayoutID : coordinator layout (aka root element)
+     * @param contentID : the content of the dropdown ( the list view and its friends)
+     */
+    private fun setUpBottomSheet(coordLayoutID: Int, contentID: Int) {
+        val coordinatorLayout = findViewById<CoordinatorLayout>(coordLayoutID)
+        val contentLayout = coordinatorLayout.findViewById<LinearLayout>(contentID)
+
+        sheetBehavior = BottomSheetBehavior.from(contentLayout)
+        sheetBehavior.setFitToContents(false)
+        sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        sheetBehavior.setHideable(false)
+
+        //Pressing the filter icon opens up the search option and filters
+        val filterIcon = findViewById<ImageView>(R.id.map_filterIcon)
+        filterIcon.setOnClickListener( View.OnClickListener { toggleFilters() })
     }
 }
